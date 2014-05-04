e5dcb44b7895409c7e50a29bf8cd56519b9adfec
Brandon Michael Kiefer <brandonmichaelkiefer@wireless-nat-inside.uiowa.edu>
2014-05-04 17:19:26 -0500

    added follow logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,28 @@ before_save :ensure_authentication_token
          :recoverable, :rememberable, :trackable, :validatable,
          :token_authenticatable,:confirmable, :lockable
 
+has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+has_many :followed_users, through: :relationships, source: :followed
+has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+has_many :followers, through: :reverse_relationships, source: :follower
+has_many :polls
+has_many :skipped_elements
+has_many :voted_ons
+
+def following?(other_user)
+  relationships.find_by(followed_id: other_user.id)
+end
+
+def follow!(other_user)
+  relationships.create!(followed_id: other_user.id)
+end
+
+def unfollow!(other_user)
+  relationships.find_by(followed_id: other_user.id).destroy
+end
+
 def skip_confirmation!
   self.confirmed_at = Time.now
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140218033705) do
+ActiveRecord::Schema.define(version: 20140504215431) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -47,6 +47,17 @@ ActiveRecord::Schema.define(version: 20140218033705) do
     t.integer  "user_id"
     t.datetime "created"
   end
+
+  create_table "relationships", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "skipped_elements", force: true do |t|
     t.integer "user_id"
