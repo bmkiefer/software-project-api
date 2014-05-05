b1c720f25d95024de4b042169073e3d817e30c65
Brandon Michael Kiefer <brandonmichaelkiefer@wireless-nat-inside.uiowa.edu>
2014-05-05 00:09:09 -0500

    clean reset password controller

--- app/controllers/api/v1/reset_password_controller.rb
+++ app/controllers/api/v1/reset_password_controller.rb
@@ -1,4 +1,4 @@
-ass Api::V1::ResetPasswordController < ApplicationController
+class Api::V1::ResetPasswordController < ApplicationController
   skip_before_filter :verify_authenticity_token,
                      :if => Proc.new { |c| c.request.format == 'application/json' }
 
